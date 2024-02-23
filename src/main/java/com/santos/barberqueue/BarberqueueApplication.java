3fcb54f621f0267ce8e6dccf68af4d821973ad30
Caio Santos <caiosant10@gmail.com>
2024-02-23 14:11:30 -0300

    Start project

---
+++ src/main/java/com/santos/barberqueue/BarberqueueApplication.java
@@ -0,0 +1,13 @@
+package com.santos.barberqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santos/barberqueue/BarberqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.barberqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
