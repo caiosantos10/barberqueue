999b5a79c7a401da10865d79d1802b9298462b6d
Caio Santos <caiosant10@gmail.com>
2024-02-25 17:38:51 -0300

    database config

--- src/main/java/com/santos/barberqueue/domain/Queue.java
+++ src/main/java/com/santos/barberqueue/domain/Queue.java
@@ -1,10 +1,32 @@
 package com.santos.barberqueue.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Queue {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Queue implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
 	private ArrayList<String> customers = new ArrayList<>();
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public List<String> getCustomers() {
 		return customers;
